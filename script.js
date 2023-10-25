98d2dbf084c78edd21f1273efc626f29d4627c3d
Mateo-SQT <128026890+Mateo-SQT@users.noreply.github.com>
2023-10-25 11:54:28 +0200

    First Upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggleButton = document.getElementById("toggleButton");
+const elementToToggle = document.getElementById("elementToToggle");
+
+toggleButton.addEventListener("click", function() {
+    if (elementToToggle.style.display === "none" || elementToToggle.style.display === "") {
+        elementToToggle.style.display = "block"; // Afficher l'élément
+    } else {
+        elementToToggle.style.display = "none"; // Masquer l'élément
+    }
+});
+
+
